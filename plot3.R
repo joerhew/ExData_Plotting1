6932a174e6be77fdd4f0d76ec5ae1ed60c31626b
Joe Rhew <joerhew@gmail.com>
2015-06-04 17:56:53 +0900

    final commit

--- plot3.R
+++ plot3.R
@@ -1,5 +1,10 @@
-## Read in the full data from the text file
-full_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+## Download the zip file into a temp file (on a Mac)
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp, method = "curl")
+
+## Extract the data from the temp file into "full_data" and then unlink the temp file
+full_data <- read.table(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";", na.strings = "?")
+unlink(temp)
 
 # Combine the Date and Time columns into "date"
 date <- strptime(paste(full_data$Date,full_data$Time), format = "%d/%m/%Y %H:%M:%S")
@@ -8,7 +13,7 @@ date <- strptime(paste(full_data$Date,full_data$Time), format = "%d/%m/%Y %H:%M:
 full_data$Datetime <- date
 
 # Assign the period for which we will be plotting, between Feb 1 2007 and Feb 2 2007 
-plot_period <- full_data[full_data$Datetime < "2007-02-02 23:59:59 KST" & full_data$Datetime > "2007-02-01 00:00:00 KST", ]
+plot_period <- full_data[full_data$Datetime < "2007-02-02 23:59:59" & full_data$Datetime > "2007-02-01 00:00:00", ]
 
 # Call PNG device, with width and height at 480px
 png(file="plot3.png", width = 480, height = 480)
